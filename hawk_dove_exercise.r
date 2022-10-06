bd4348acff8dc2b0bbd99834f54f83ba93b9d972
Bram Kuijper <bramkuijper@gmail.com>
2022-10-06 01:22:20 +0100

    various edits based on comments by George Hancock

--- hawk_dove_exercise.r
+++ hawk_dove_exercise.r
@@ -1,17 +1,16 @@
 # broken code on the evolution of hawks vs doves
 # we intend to iterate the haploid population genetics
 # recursion p(t+1) = p(t)*...
-# but quite some things are missing or are wrong.
-# Can you spot the errors / omissions?
+# but quite some things are missing 
 
 # parameters coding the payoffs
 c <- 2.0
 
 # baseline fitness
-w0 <- -100
+w0 <- 10
 
 # maximum time the simulation should run for
-max_time <- 10
+max_time <- 1000
 
 # a vector containing allele frequencies
 # for all time steps
@@ -19,16 +18,18 @@ p <- rep(x=NA, times=max_time)
 
 # set the initial frequency of the hawks
 # at time step 1
-p[[1]] <- 0.01
-
-
-wD <- w0 + p[[time_idx]] * 0
+p[[1]] <- 0.5
 
 # a for loop to iterate the recursion equation
 # over multiple timesteps
-for (time_idx  )
+for (time_idx in )
 {
+    # calculate fitness of a dove
+    wD <- w0 + p[[time_idx]] * 0 + (1-p[[time_idx]]) * v/2
+
+    # calculate mean fitness
+    wBar <- wD * (1 - p[[time_idx]]) + wH * 
+
     # recursion equation
-    p[[time_idx + 1]] <- p[[time_idx]] * wD
 }
 
